86552d98811f35baaf3ce9f88399445a19a9498f
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-03-26 15:53:48 -0400

    Cleaned up GlobalState.js and AskQuestion.js

--- client/src/context/QuestionReducer.js
+++ client/src/context/QuestionReducer.js
@@ -5,26 +5,10 @@ export default (state, action) => {
                 ...state,
                 questions: action.payload
             }
-        case 'UPDATE_QUESTION':
+        case 'ADD_QUESTION':
             return {
                 ...state,
-                question: action.payload
-            }
-        case 'THROW_QUESTION_ERROR':
-            return {
-                ...state,
-                errors: {
-                    answerError: "",
-                    questionError: action.payload
-                }
-            }
-        case 'RESET_ERRORS':
-            return {
-                ...state,
-                errors: {
-                    answerError: "",
-                    questionError: ""
-                }
+                questions: [action.payload, ...state.questions]
             }
         default:
             return state;
